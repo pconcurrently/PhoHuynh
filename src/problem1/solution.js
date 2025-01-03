55cd4e7922f57ed68f1188dd865e93e38cb38e7c
Pho Huynh <p@ccly.dev>
2025-01-03 18:12:00 +0700

    feature: Problem 1

---
+++ src/problem1/solution.js
@@ -0,0 +1,19 @@
+// Using math formula: sum = n * (n + 1) / 2
+var sum_to_n_a = function (n) {
+	return (n * (n + 1)) / 2;
+};
+
+// Using iteration/loop approach
+var sum_to_n_b = function (n) {
+	let sum = 0;
+	for (let i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+};
+
+// Using recursive approach
+var sum_to_n_c = function (n) {
+	if (n <= 0) return 0;
+	return n + sum_to_n_c(n - 1);
+};
